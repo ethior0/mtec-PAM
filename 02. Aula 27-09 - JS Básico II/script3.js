0cf59cb40d96ac35368fc7307f5b08716347bcb7
thiago.dsm <133548984+ethior0@users.noreply.github.com>
2024-10-01 23:46:06 -0300

    Create script3.js

---
+++ 02. Aula 27-09 - JS Básico II/script3.js
@@ -0,0 +1,35 @@
+var a = 6;
+var b = 2;
+ 
+function calculoAdicao(){
+   
+    var c = a+b;
+    alert(a +'+'+ b + '= '+ c);
+}
+ 
+calculoAdicao();
+ 
+function calculoSubtracao(){
+   
+    var c = a-b;
+    alert(a +'-'+ b + '= '+ c);
+ 
+}
+ 
+calculoSubtracao();
+ 
+function calculoMulti(){
+   
+    var c = a*b;
+    alert(a +'x'+ b + '= '+ c);
+}
+ 
+calculoMulti();
+ 
+function calculoDivisao(){
+   
+    var c = a/b;
+    alert(a +'÷'+ b + '= '+ c);
+}
+ 
+calculoDivisao();
